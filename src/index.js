fc2b0dc6ec6d6accae4ef0ba245a1b87a8e0667b
ljh <ljh_xx@163.com>
2017-12-15 14:38:24 +0800

    HMR

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import _ from 'lodash';
-import './index.css';
-import Icon from './icon.jpg';
+import printMe from './print.js';
+import './style.css';
+import { cube } from './math.js';
 
 function component() {
 	var element = document.createElement('div');
+	var btn = document.createElement('button');
 
-	// Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
+	element.innerHTML = [
+		'Hello webpack!',
+		'5 cubed is equal to ' + cube(5)
+	].join('\n\n');
 
-	var myIcon = new Image();
-	myIcon.src = Icon;
-	element.appendChild(myIcon);
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;  // onclick event is bind to the original printMe function
+
+	element.appendChild(btn);
 
 	return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+	module.hot.accept('./print.js', function () {
+		console.log('Accepting the updated printMe module!');
+		document.body.removeChild(element);
+		element = component(); // Re-render the "component" to update the click handler
+		document.body.appendChild(element);
+	})
+}
